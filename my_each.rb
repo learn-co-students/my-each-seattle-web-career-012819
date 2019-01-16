a76c05a755c3618cf37473d9c0dcee17e8bfc83d
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-16 03:28:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,12 @@
 def my_each(array)
   if block_given?
     i = 0 
+    returned_array = []
     while i < array.length 
       yield(array[i])
+      returned_array << array
       i += 1 
     end 
   end
+  returned_array
 end
